c36c964063d8f968b887923954b98e8ab1508188
josephKimiri <joseph.kimiri.macharia@gmail.com>
2022-03-04 16:30:13 +0300

    Change the databases name.

--- includes/config.php
+++ includes/config.php
@@ -3,7 +3,7 @@
 define('DB_HOST','localhost');
 define('DB_USER','root');
 define('DB_PASS','');
-define('DB_NAME','carrental');
+define('DB_NAME','carrentals');
 // Establish database connection.
 try
 {
